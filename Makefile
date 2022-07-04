221bd0f2b29399a169e76ec9eece63b701c177a7
ShiyuCheng2018 <starkeyshiyu@gmail.com>
2022-07-04 01:33:10 -0700

    [feat] GetUserList

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 
-
+GOPATH:=$(shell go env GOPATH)
+.PHONY: init
+init:
+	go install github.com/golang/protobuf/protoc-gen-go@latest
 .PHONY: proto
 proto:
 	  protoc -I ./user-web/proto ./user-web/proto/user.proto --go_out=plugins=grpc:.
--- user-web/api/user.go
+++ user-web/api/user.go
@@ -1,10 +1,86 @@
 package api
 
 import (
+	"context"
+	"fmt"
+	"github.com/ShiyuCheng2018/mxshop-api/user-web/proto"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"net/http"
 )
 
+func HandleGrpcErrorToHttpError(err error, c *gin.Context) {
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.NotFound:
+				c.JSON(http.StatusNotFound, gin.H{
+					"msg": e.Message(),
+				})
+			case codes.Internal:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "[Internal Server Error]",
+				})
+			case codes.InvalidArgument:
+				c.JSON(http.StatusBadRequest, gin.H{
+					"msg": "[Invalid Argument]",
+				})
+			case codes.Unavailable:
+				c.JSON(http.StatusServiceUnavailable, gin.H{
+					"msg": "[Service Unavailable]",
+				})
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": e.Message(),
+				})
+			}
+			return
+		}
+	}
+}
+
 func GetUserList(ctx *gin.Context) {
-	zap.S().Debug("[GetUserList]: Get user list")
+	ip := "127.0.0.1"
+	port := 50051
+
+	// connect to client grpc server
+	userConnection, err := grpc.Dial(fmt.Sprintf("%s:%d", ip, port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		zap.S().Errorw("[GetUserList]: Failed to connect")
+		HandleGrpcErrorToHttpError(err, ctx)
+	}
+
+	// get grpc client
+	userSrvClient := proto.NewUserClient(userConnection)
+
+	response, err := userSrvClient.GetUserList(context.Background(), &proto.PageInfo{
+		Page: 0,
+		Size: 0,
+	})
+	if err != nil {
+		zap.S().Errorw("[GetUserList]: Failed to get user list")
+		HandleGrpcErrorToHttpError(err, ctx)
+		return
+	}
+
+	result := make([]interface{}, 0)
+	for _, val := range response.Data {
+		data := make(map[string]interface{})
+
+		data["id"] = val.Id
+		data["name"] = val.Name
+		data["nickname"] = val.Nickname
+		data["birthday"] = val.Birthday
+		data["gender"] = val.Gender
+		data["mobile"] = val.Mobile
+		data["role"] = val.Role
+		result = append(result, data)
+	}
+
+	ctx.JSON(http.StatusOK, result)
+
 }
